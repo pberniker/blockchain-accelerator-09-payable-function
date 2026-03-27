dd3d75f7a4577f0604b43a8f62bc1c2dfa1df1f5
pabloberniker <pberniker@digbang.com>
2026-03-27 08:58:01 -0300

    09-init

---
+++ Contracts/PayableContract.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0-only
+
+pragma solidity 0.8.24;
+
+contract PayableContract {
+    // 1 ETH = 1*10^18 WEI
+
+    // Smart contact balance
+
+    // Functions
+
+    // Externals
+
+    function sendEther() public payable {
+    }
+
+    // No se puede retirar => Vulnerabilidad
+}
