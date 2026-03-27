d3551b9e2aa09243fc748c9000fc43d3c1b2c8ce
pabloberniker <pberniker@digbang.com>
2026-03-27 09:04:47 -0300

    09-init

--- Contracts/PayableContract.sol
+++ Contracts/PayableContract.sol
@@ -3,6 +3,8 @@
 pragma solidity 0.8.24;
 
 contract PayableContract {
+    // Para recibir ETH: receive(), fallback(), payable function o out code.
+
     // 1 ETH = 1*10^18 WEI
 
     // Smart contact balance
